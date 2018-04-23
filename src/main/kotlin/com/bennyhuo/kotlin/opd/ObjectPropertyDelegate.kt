da09c3866579d45c56602d7215e9ba703305645a
bennyhuo <bennyhuo@kotliner.cn>
2018-04-23 23:24:29 +0800

    Use overload instead of if ... else ...

--- src/main/kotlin/com/bennyhuo/kotlin/opd/ObjectPropertyDelegate.kt
+++ src/main/kotlin/com/bennyhuo/kotlin/opd/ObjectPropertyDelegate.kt
@@ -4,10 +4,11 @@ import kotlin.jvm.internal.PropertyReference
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.*
 
-fun <T> delegateOf(getter: (() -> T)? = null, setter: ((T) -> Unit)? = null, defaultValue: T? = null): ReadWriteProperty<Any, T>
-        = if(getter == null) ObjectPropertyDelegateNoGetter0(setter, defaultValue)  else ObjectPropertyDelegate0(getter, setter, defaultValue)
-fun <T, R> delegateWithReceiverOf(receiver: R, getter: ((R) -> T)? = null, setter: ((R, T) -> Unit)? = null, defaultValue: T? = null): ReadWriteProperty<Any, T>
-        = if(getter == null) ObjectPropertyDelegateNoGetter1(receiver, setter, defaultValue)  else ObjectPropertyDelegate1(receiver, getter, setter, defaultValue)
+fun <T> delegateOf(setter: ((T) -> Unit)? = null, defaultValue: T? = null): ReadWriteProperty<Any, T> = ObjectPropertyDelegateNoGetter0(setter, defaultValue)
+fun <T> delegateOf(getter: (() -> T), setter: ((T) -> Unit)? = null, defaultValue: T? = null): ReadWriteProperty<Any, T> = ObjectPropertyDelegate0(getter, setter, defaultValue)
+
+fun <T, R> delegateWithReceiverOf(receiver: R, setter: ((R, T) -> Unit)? = null, defaultValue: T? = null): ReadWriteProperty<Any, T> = ObjectPropertyDelegateNoGetter1(receiver, setter, defaultValue)
+fun <T, R> delegateWithReceiverOf(receiver: R, getter: ((R) -> T), setter: ((R, T) -> Unit)? = null, defaultValue: T? = null): ReadWriteProperty<Any, T> = ObjectPropertyDelegate1(receiver, getter, setter, defaultValue)
 
 fun <T> KProperty0<T>.delegator(defaultValue: T? = null): ReadWriteProperty<Any, T> = ObjectPropertyDelegate0(propertyRef = this as PropertyReference, defaultValue = defaultValue)
 fun <T, R> KProperty1<R, T>.delegator(receiver: R, defaultValue: T? = null): ReadWriteProperty<Any, T> = ObjectPropertyDelegate1(receiver, property = this, defaultValue = defaultValue)
@@ -16,16 +17,16 @@ fun <T, R> KProperty1<R, T>.delegator(receiver: R, defaultValue: T? = null): Rea
 fun <T> KFunction0<T>.delegator(defaultValue: T? = null): ReadWriteProperty<Any, T> = ObjectPropertyDelegate0(this, defaultValue = defaultValue)
 
 @JvmName("delegatorGetter")
-fun <T, R> KFunction1<R, T>.delegator(receiver: R, defaultValue: T? = null): ReadWriteProperty<Any, T> = ObjectPropertyDelegate1(receiver,this, defaultValue = defaultValue)
+fun <T, R> KFunction1<R, T>.delegator(receiver: R, defaultValue: T? = null): ReadWriteProperty<Any, T> = ObjectPropertyDelegate1(receiver, this, defaultValue = defaultValue)
 
 fun <T> KFunction1<T, Unit>.delegator(defaultValue: T? = null): ReadWriteProperty<Any, T> = ObjectPropertyDelegateNoGetter0(setter = this, defaultValue = defaultValue)
 fun <T, R> KFunction2<R, T, Unit>.delegator(receiver: R, defaultValue: T? = null): ReadWriteProperty<Any, T> = ObjectPropertyDelegateNoGetter1(receiver, setter = this, defaultValue = defaultValue)
 
-private class ObjectPropertyDelegateNoGetter0<T>(val setter: ((T) -> Unit)? = null, defaultValue: T? = null): ReadWriteProperty<Any, T> {
+private class ObjectPropertyDelegateNoGetter0<T>(val setter: ((T) -> Unit)? = null, defaultValue: T? = null) : ReadWriteProperty<Any, T> {
     private var value: T? = defaultValue
 
     init {
-        defaultValue?.let {  setter?.invoke(it) }
+        defaultValue?.let { setter?.invoke(it) }
     }
 
     override operator fun getValue(thisRef: Any, property: KProperty<*>): T {
@@ -38,13 +39,13 @@ private class ObjectPropertyDelegateNoGetter0<T>(val setter: ((T) -> Unit)? = nu
     }
 }
 
-private class ObjectPropertyDelegate0<T>(val getter: (() -> T), val setter: ((T) -> Unit)? = null, defaultValue: T? = null): ReadWriteProperty<Any, T> {
+private class ObjectPropertyDelegate0<T>(val getter: (() -> T), val setter: ((T) -> Unit)? = null, defaultValue: T? = null) : ReadWriteProperty<Any, T> {
 
     constructor(propertyRef: PropertyReference, defaultValue: T? = null)
             : this((propertyRef as KProperty0<T>)::get, if (propertyRef is KMutableProperty0<*>) (propertyRef as KMutableProperty0<T>)::set else null, defaultValue)
 
     init {
-        defaultValue?.let {  setter?.invoke(it) }
+        defaultValue?.let { setter?.invoke(it) }
     }
 
     override operator fun getValue(thisRef: Any, property: KProperty<*>): T {
@@ -60,7 +61,7 @@ private class ObjectPropertyDelegateNoGetter1<T, R>(val receiver: R, val setter:
     private var value: T? = defaultValue
 
     init {
-        defaultValue?.let {  setter?.invoke(receiver, it) }
+        defaultValue?.let { setter?.invoke(receiver, it) }
     }
 
     override operator fun getValue(thisRef: Any, property: KProperty<*>): T {
@@ -79,7 +80,7 @@ private class ObjectPropertyDelegate1<T, R>(val receiver: R, val getter: ((R) ->
             : this(target, property, if (property is KMutableProperty1<*, *>) (property as KMutableProperty1<R, T>)::set else null, defaultValue)
 
     init {
-        defaultValue?.let {  setter?.invoke(receiver, it) }
+        defaultValue?.let { setter?.invoke(receiver, it) }
     }
 
     override operator fun getValue(thisRef: Any, property: KProperty<*>): T {
